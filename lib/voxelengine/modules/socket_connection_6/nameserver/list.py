07242b39de9a95730b4bd83979c88e3f07d4aef8
Joram Brenz <joram.brenz@online.de>
2020-08-01 16:42:52 +0200

    added nameserver scripts to project

---
+++ lib/voxelengine/modules/socket_connection_6/nameserver/list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import time
+import json
+import os, inspect
+PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+TTL = 60
+
+#import cgitb
+#cgitb.enable()
+#print("Content-Type:text/html")
+#print()
+
+import cgi
+form = cgi.FieldStorage()
+
+if "key" in form:
+	key = form["key"].value
+else:
+	key = "voxelgame"
+
+import sqlite3
+dbpath = os.path.join(PATH, "database", "server.db")
+conn = sqlite3.connect(dbpath)
+c = conn.cursor()
+
+c.execute('''
+SELECT * FROM server
+WHERE time > ? AND key = ?
+''',(time.time()-TTL, key))
+output = json.dumps(tuple(value for key, value, timestamp in c.fetchall()))
+conn.close()
+
+print("Content-Type: text/json")
+print("Access-Control-Allow-Origin: *")
+print()
+print(output)
+
---
+++ lib/voxelengine/modules/socket_connection_6/nameserver/register.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+import time
+import socket
+import json
+import os
+import inspect
+PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+#import cgitb
+#cgitb.enable()
+
+import cgi
+print("Content-Type: text/html")
+print()
+
+import cgi
+form = cgi.FieldStorage()
+
+if "key" not in form:
+	print("missing parameter 'key'")
+	exit(0)
+if "value" not in form:
+	print("missing parameter 'value'")
+	exit(0)
+
+ip = os.environ["REMOTE_ADDR"] #this variable should be set by caller of cgi script
+
+key   = form["key"].value
+value = form["value"].value
+
+# validate data to be mcgcraft serverinfo
+serverinfo = json.loads(value)
+if not str(serverinfo["http_port"]).isdigit():
+	print("port is not a natural number (%s)" % port)
+	exit(0)
+if ip != socket.gethostbyname(serverinfo["host"]):
+	print("hostname does not resolve to request origin (%s)" % ip)
+	exit(0)
+
+
+import sqlite3
+dbpath = os.path.join(PATH, "database", "server.db")
+conn = sqlite3.connect(dbpath)
+c = conn.cursor()
+
+if False:
+	c.execute('''DROP TABLE server''')
+	# Create table
+	c.execute('''CREATE TABLE server
+				 (key text, value text, time real, PRIMARY KEY (key, value))''')
+
+if False:
+	# drop entries older than one minute
+	c.execute('''DELETE FROM server
+	             WHERE time < ?''', (time.time()-60,))
+
+# Insert a row of data
+c.execute("REPLACE INTO server VALUES (?,?,?)", (key,value,time.time()))
+
+# Save (commit) the changes
+conn.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
+
+print("success")
