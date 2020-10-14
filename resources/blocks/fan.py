bac20f1a7f69ac4ca49c30cdb8c3349a6824517d
Brenz <brenzj>
2020-10-14 17:05:05 +0200

    fan only trying to push once per tick; maybe add decorator?

--- resources/blocks/fan.py
+++ resources/blocks/fan.py
@@ -8,9 +8,11 @@ class fanblock(Block):
         return super().get_tags() + {"block_update"}
 
     def handle_event_block_update(self,event):
-        if self.redstone_activated():
+        if self.redstone_activated() and self.get("last_push",0) < self.world.clock.current_gametick:
             direction = -self.get_base_vector()
             p1 = self.position + direction
             p2 = p1 + direction
             if self.world.blocks[p1] != "AIR" and self.world.blocks[p2] == "AIR":
                 self.world.request_move_block(p1,p2)
+                self["last_push"]=self.world.clock.current_gametick
+                self.save()
