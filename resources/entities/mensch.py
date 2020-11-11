1ce4f1c6cf9457e2ec2bddd9f9eed5d960dfa8c5
Shildifreak <joram.brenz@online.de>
2020-11-11 14:29:18 +0100

    fixed block maps not being json

--- resources/entities/mensch.py
+++ resources/entities/mensch.py
@@ -1,5 +1,6 @@
 from resources import *
 import time, random
+import json
 
 LEGS = ["missing_texture", "TESTBLOCK", "SAND", "SANDSTEIN", "BRICK", "BEDROCK", "DIRT", "STONE", "MOSSSTONE", "HOLZ", "LAUB", "HOLZBRETTER", "GRASS", "Myzeln", "SCHNEEERDE", "BRUCHSTEIN", "SCHNEE", "SCHILDBLOCK", "GELB", "HELLGRUN", "GRUN", "TURKIES", "DUNKELGRUN", "DUNKELGRAU", "LILA", "GRAU", "ROT", "HELLROT", "ROSA", "SCHWARZ", "BRAUN", "ORANGE", "HELLORANGE", "HELLBLAU", "HELLGRAU", "WEISS", "DIAMANT", "DIAMANTERZ", "GOLD", "GOLDERZ", "IRON", "EISENERZ", "KOHLEERZ", "REDSTONE", "Redstone", "SMARAGTBLOCK", "SMARAGTERZ", "LAPIS", "Lapiserz", "GESICHT", "Damengesicht", "CREEPER", "Zombie", "Skelett", "Schaf", "Geist", "Schafbeine", "Menschenbeine", "Damenbeine", "Damenkoerper", "Commandblock", "LAMPOFF", "LAMPON", "FAN", "Repeater", "Piston", "RACKETENWERFER", "TNT", "A-TNT", "B-TNT", "Fruhlingsgrass", "Fruhlingslaub", "Quarzsaule", "GLAS", "Netherstone", "Bucherregal", "Obsidian", "Prismarin", "Rissige_Steinziegel", "Steinziegel", "STEINZIEGEL", "Gemeisselter_Steinziegel", "CHEST", "DOORTOP", "DOORBOTTOM", "DOORTOPOPEN", "DOORBOTTOMOPEN", "DOORSTEP", "BARRIER", "WAND", "AIM", "KAKTUS"]
 EMOTES = ["GESICHT_SMILE:2", "GESICHT_HOLLOW_EYES:2"]
@@ -14,7 +15,7 @@ class Mensch(Entity):
     def __init__(self, *args, **kwargs):
         super(Mensch,self).__init__()
 
-        self["skin"] = repr({"<<random>>":random.choice(LEGS)}).replace(" ","")
+        self["skin"] = random.choice(LEGS)
         self["emote"] = EMOTES[0]
         self.register_item_callback(lambda _:self.update_texture(),"skin")
 
@@ -32,7 +33,7 @@ class Mensch(Entity):
         texture_mappings = {"<<random>>":self["skin"]}
         if show_emote:
             texture_mappings["GESICHT:2"] = self["emote"]
-        self["texture"] = "MENSCH:%s" % repr(texture_mappings).replace(" ","")
+        self["texture"] = "MENSCH:%s" % json.dumps(texture_mappings, separators=(',',':'))
 
     def select_emote(self, index):
         self["emote"] = EMOTES[index%len(EMOTES)]
