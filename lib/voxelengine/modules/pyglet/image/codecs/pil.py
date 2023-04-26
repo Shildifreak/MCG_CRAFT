a3fa0f202351eea17f078549254f738db44481cd
Joram Brenz <joram.brenz@online.de>
2023-04-26 13:20:59 +0200

    fix from pyglet/pyglet@569cbb501caa4d36486418c089005da5ba81102f

--- lib/voxelengine/modules/pyglet/image/codecs/pil.py
+++ lib/voxelengine/modules/pyglet/image/codecs/pil.py
@@ -81,7 +81,10 @@ class PILImageDecoder(ImageDecoder):
 
         # tostring is deprecated, replaced by tobytes in Pillow (PIL fork)
         # (1.1.7) PIL still uses it
-        image_data_fn = getattr(image, "tobytes", getattr(image, "tostring"))
+        try:
+            image_data_fn = getattr(image, "tobytes")
+        except AttributeError:
+            image_data_fn = getattr(image, "tostring")
         return ImageData(width, height, image.mode, image_data_fn())
 
 class PILImageEncoder(ImageEncoder):
@@ -107,7 +110,10 @@ class PILImageEncoder(ImageEncoder):
 
         # fromstring is deprecated, replaced by frombytes in Pillow (PIL fork)
         # (1.1.7) PIL still uses it
-        image_from_fn = getattr(Image, "frombytes", getattr(Image, "fromstring"))
+        try:
+            image_from_fn = getattr(Image, "frombytes")
+        except AttributeError:
+            image_from_fn = getattr(Image, "fromstring")
         pil_image = image_from_fn(
             format, (image.width, image.height), image.get_data(format, pitch))
 
