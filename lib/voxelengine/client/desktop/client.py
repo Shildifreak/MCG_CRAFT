73431e374cbaa23441eb97d516c697c4de82cbc0
Joram Brenz <joram.brenz@online.de>
2023-11-13 21:05:20 +0100

    more shader examples

--- lib/voxelengine/client/desktop/client.py
+++ lib/voxelengine/client/desktop/client.py
@@ -117,17 +117,21 @@ vertex_shader_code = b"""
 #version 130
 
 varying vec4 color;
+varying vec4 viewpos;
 
 void main()
 {
     //normal = gl_NormalMatrix * gl_Normal;
     color = gl_Color;
-    gl_Position = ftransform();
+    gl_Position = ftransform(); // fixed pipeline equivalent of gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;
     gl_TexCoord[0] = gl_MultiTexCoord0;
 
     // fog stuff (https://community.khronos.org/t/opengl-fog-and-shaders/52902)
     vec4 eyePos = gl_ModelViewMatrix * gl_Vertex;
     gl_FogFragCoord = abs(eyePos.z/eyePos.w);
+    
+    // more info for fragment shaders to play with
+    viewpos = gl_ModelViewMatrix * gl_Vertex;
 }
 """
 
