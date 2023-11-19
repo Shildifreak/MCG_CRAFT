e9528795a456f6a87f11e788be5fc0ec47569337
Joram Brenz <joram.brenz@online.de>
2023-11-19 18:51:15 +0100

    added normal hack to vertex shader; using up vector instead of missing vertex normal and modelViewMatrix instead of deprecated normalMatrix

--- lib/voxelengine/client/desktop/client.py
+++ lib/voxelengine/client/desktop/client.py
@@ -119,10 +119,15 @@ vertex_shader_code = b"""
 
 varying vec4 color;
 varying vec4 viewpos;
+varying vec4 normal;
 
 void main()
 {
     //normal = gl_NormalMatrix * gl_Normal;
+    // hack: using up vector only works on top surface of blocks
+    // notice: there are issues with using modelViewMatrix instead of proper normalMatrix when doing non-uniform scale
+    normal = gl_ModelViewMatrix * vec4(0,1,0,0);
+
     color = gl_Color;
     gl_Position = ftransform(); // fixed pipeline equivalent of gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;
     gl_TexCoord[0] = gl_MultiTexCoord0;
