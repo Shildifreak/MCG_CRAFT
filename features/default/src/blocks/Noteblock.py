d61e5e139f7514342711e5e174e1af6c378a01f1
Joram Brenz <joram.brenz@online.de>
2024-11-27 02:19:14 +0100

    allow blocks to have none or multiple place/break sounds playing at once make Noteblocks more stable by waiting for completed tick before reacting

--- features/default/src/blocks/Noteblock.py
+++ features/default/src/blocks/Noteblock.py
@@ -4,14 +4,22 @@ from resources import *
 class Noteblock(Block):
 
     def get_tags(self):
-        return super().get_tags() | {"block_update"}
+        return super().get_tags() | {"block_update","delayed_update"}
 
-    def handle_event_block_update(self,event):
-        if self.redstone_activated() and self.get("last_played",0) + 20 < self.world.clock.current_gametick:
+    def handle_event_block_update(self,_):
+        self.world.event_system.add_event(Event("delayed_update",Point(self.position),None))
+        return False
+        
+    def handle_event_delayed_update(self,_):
+        if self.redstone_activated(-1) and self.get("last_played",0) + 5 < self.world.clock.current_gametick:
             self["last_played"]=self.world.clock.current_gametick
 
-            sound = self.relative[(0,-1,0)].get_break_sound()
-            sound_event = Event("sound",Point(self.position),sound)
-            self.world.event_system.add_event(sound_event)
+#            sounds = self.relative[(0,1,0):-1].get_break_sounds()
+            source = self.world.blocks.get(self.position+(0,1,0), t=-1)
+            sounds = source.get_break_sounds()
+            for sound in sounds:
+                print(sound)
+                sound_event = Event("sound",Point(self.position),sound)
+                self.world.event_system.add_event(sound_event)
             return True
         return False
